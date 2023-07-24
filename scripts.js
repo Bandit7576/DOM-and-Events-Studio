947e2a051da3c5fbea734fd3341232715487a639
Wilber Hasty <wilberhasty75@gmail.com>
2023-07-23 21:18:32 -0500

    First commit on project

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 
-window.addEventListener("load", fucntion() )
+window.addEventListener("load", function() {
+
+    const takeoffButton = document.getElementById('takeoff');
+
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById('shuttleBackground');
+    const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
+
+
+    takeoffButton.addEventListener("click", function() {
+        let shouldTakeOff = confirm("Confirm that the shuttle is ready for takeoff");
+
+        if (shouldTakeOff) {
+            flightStatus.innerHTML = "Shuttle in flight";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10000";
+        }
+    });
+});
